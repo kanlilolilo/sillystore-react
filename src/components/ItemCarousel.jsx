9ef69a4bdef835c56500fec6507c62f35b8d5f61
Meow <108752066+Kansloos12345@users.noreply.github.com>
2025-05-14 09:55:45 +0200

    betere product page UPDATE

--- src/components/ItemCarousel.jsx
+++ src/components/ItemCarousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import testimg from '../assets/img/clothing_img/shirt/cat_withshirt.png';
+import testimg from '../assets/img/clothing_img/shirt/cat_withshirt_small.png';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -11,46 +11,39 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
-import { Zoom, Pagination, Autoplay } from 'swiper/modules';
+import { Pagination, Autoplay, Navigation } from 'swiper/modules';
+
 
 export default function App() {
   return (
     <>
-      <div className="lg:pt-12">
+      <div className="">
         <Swiper
           style={{
-            '--swiper-navigation-color': '#fff',
-            '--swiper-pagination-color': '#fff',
+            '--swiper-navigation-color': '#000',
+            '--swiper-pagination-color': '#000',
           }}
           zoom={true}
           spaceBetween={0}
           centeredSlides={true}
-          pagination={{
-            dynamicBullets: true,
-          }}
+          navigation={true}
           autoplay={{
             delay: 10000,
             disableOnInteraction: false,
           }}
-          modules={[Pagination, Autoplay]}
+          modules={[Pagination, Autoplay, Navigation]}
         >
           {/* big slides */}
           <SwiperSlide className="swiper-zoom-container">
-            <a href="" className="cursor-default">
-              <img src={testimg} alt="Slide 1" className="w-auto sm:h-[480px] sm:w-auto bg-[#fff]"/>
-            </a>
+              <img src={testimg} alt="Slide 1" className="bg-[#fff]"/>
           </SwiperSlide>
           
           <SwiperSlide className="swiper-zoom-container">
-            <a href="https://www.rvspijker.nl/" className="cursor-default">
-              <img src={testimg} alt="Slide 1" className="w-auto sm:h-[480px] sm:w-auto bg-[#fff]"/>
-            </a>
+              <img src={testimg} alt="Slide 1" className="bg-[#fff]"/>
           </SwiperSlide>
 
           <SwiperSlide className="swiper-zoom-container">
-            <a href="" className="cursor-default">
-              <img src={testimg} alt="Slide 1" className="w-auto sm:h-[480px] sm:w-auto bg-[#fff]"/>
-            </a>
+              <img src={testimg} alt="Slide 1" className="bg-[#fff]"/>
           </SwiperSlide>
           
           
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,52 +1,46 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { SpotifyContext } from '../SpotifyContext';
+import React from 'react';
 import ItemCarousel from './ItemCarousel';
 import styles from './Styles.module.css';
-import Button from '@mui/material/Button';
+import wishlist from '../assets/img/heart_img.png';
 
 function Product() {
   return (
-    <div className="overflow-hidden mb-8">
-      <div className="lg:flex overflow-hidden">
-        <div className="flex-1 h-full overflow-hidden">
-          <ItemCarousel />
+    <div>
+      <div className="relative">
+        <ItemCarousel />
+        <div className="absolute md:top-4 md:left-6 top-0 left-0 px-4 py-2 rounded-md z-10 rendering-pixelated flex gap-4">
+          <img width="48px" height="48px" className="cursor-pointer" src={wishlist}></img>
+          <p className="max-md:hidden">← I LOVE</p>
         </div>
-        <div className="flex flex-col justify-center flex-1 sm:gap-8 gap-4 max-sm:mt-4">
-          <div className={`sm:p-6 p-3 sm:mx-8 mx-2`}>
-            <h1 className="sm:text-3xl text-lg font-bold">Super Coole tshirt</h1>
-          </div>
-          <div className={`sm:p-6 p-3 flex-1 sm:mx-8 mx-2`}>
-            <h1 className="text-xl font-bold">Holy Moley!</h1>
-          </div>
-          <div className="flex flex-row gap-2 sm:mx-8 mx-2">
-            <div className={`cursor-pointer flex-grow`}>
-              <Button
-                variant="contained"
-                style={{
-                  backgroundColor: '#3d58ba',
-                  color: '#fff',
-                }}
-                className="w-full h-10 sm:h-12"
-              >
-                NU KOPEN PLS PLS
-              </Button>
-            </div>
-            <div className={`cursor-pointer`}>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: '#dd125f',
-                color: '#fff',
-              }}
-              className="w-full h-10 sm:h-12"
-            >
-              Add to cart :3
-            </Button>
-            </div>
-          </div>
+        <h1 className="absolute md:top-4 md:right-6 top-0 right-0 text-2xl font-bold bg-black/60 px-4 py-2 rounded-md z-10">
+          $18.99
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap justify-center lg:gap-16 gap-8 p-4 mt-8">
+        <div className="w-[320px] bg-[#defeff] outline outline-[#3d58ba] shadow-md p-6 rounded-lg">
+          <h2 className="text-2xl font-semibold mb-2">Super mooie tshirt van je moeder</h2>
+          <p className="text-lg">dit is echt fantastisch kharua skibidi rvspijker haha gay oke en nu wtf doe ik hallo wanneer fortnite</p>
+        </div>
+        <div className="w-[320px] bg-[#defeff] outline outline-[#3d58ba] shadow-md p-6 rounded-lg">
+          <h2 className="text-2xl font-semibold mb-2">Maten enzo haha</h2>
+          <p className="text-lg">XXS XS S M L XL XXL XXXL XXXXL EN ZELFS XXXXXL voor de discord mods</p>
+        </div>
+        <div className="w-[320px] bg-[#defeff] outline outline-[#3d58ba] shadow-md p-6 rounded-lg">
+          <h2 className="text-2xl font-semibold mb-2">Wordt ASAP (neit) geleverd met postNL</h2>
+          <p className="text-lg">Gast denk je nou echt dat dit bezorgt gaat worden?</p>
         </div>
       </div>
+
+      <div class="flex justify-center gap-6 mt-6 mb-4">
+        <button class="px-6 py-3 text-white text-xl font-bold bg-gradient-to-r from-pink-500 via-green-400 to-yellow-300 border-4 border-black rounded-xl shadow-lg shadow-pink-500 animate-bounce hover:rotate-6 transition-all duration-300">
+          BUY NOW!!!
+        </button>
+        <button class="px-6 py-3 text-white text-xl font-bold bg-gradient-to-r from-yellow-300 via-blue-400 to-pink-500 border-4 border-black rounded-xl shadow-lg shadow-blue-500 animate-pulse hover:-rotate-6 transition-all duration-300">
+          In cart pls
+        </button>
+      </div>
+
     </div>
   );
 }
