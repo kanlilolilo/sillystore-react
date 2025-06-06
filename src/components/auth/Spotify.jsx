e4a49396ede1f4f05434ec3df276a3fc82252759
Meow <108752066+Kansloos12345@users.noreply.github.com>
2025-06-06 10:33:27 +0200

    login page demo

--- src/components/auth/Spotify.jsx
+++ src/components/auth/Spotify.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import phplogo from '../../assets/img/new-php-logo.svg';
 function SpotifyLogin() {
 
   const navigate = useNavigate();
@@ -11,23 +11,33 @@ function SpotifyLogin() {
 
   return (
     <div>
-      <h1 className="text-3xl mx-auto text-center mb-6">Login</h1>
-      
-      <div className="my-16">
-        <a href="http://localhost:8888/" className="hover:underline cursor-pointer text-center text-2xl">
-          <p>Log in with Spotify :3</p>
-        </a>
-      </div>
-
-      <div className="text-center">
-        <div className="mb-4">
-          <a onClick={goToLogin} className="hover:underline cursor-pointer">
-            Log in normally (boring)
+      <h1 className="text-3xl max-lg:text-2xl mx-auto text-center mb-12 mt-3">
+        How do you want to login :3 :3 :3 :3 ;3
+      </h1>
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-16 mb-12">
+        {/* Spotify Card */}
+        <div className="flex flex-col items-center">
+          <a
+            href="http://localhost:8888/"
+            className="w-[240px] h-[240px] bg-[#fff] border border-4 flex flex-col justify-center items-center hover:shadow-lg transition-shadow"
+          >
+            {/* Spotify SVG */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#1ED760" class="bi bi-spotify" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.669 11.538a.5.5 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686m.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858m.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288"/>
+            </svg>
           </a>
+          <p className="text-xl text-center mt-4">Log in with Spotify :3</p>
         </div>
-        <a href="https://spotify.com/en/login" className="hover:underline cursor-pointer">
-          Don't have a Spotify account? Sign up here :3
-        </a>
+        {/* Normal Login Card */}
+        <div className="flex flex-col items-center">
+          <button
+            onClick={goToLogin}
+            className="w-[240px] h-[240px] bg-[#fff] border border-4 flex flex-col justify-center items-center hover:shadow-lg transition-shadow"
+          >
+            <img className="h-16" src={phplogo}></img>
+          </button>
+          <span className="text-xl text-center mt-4">Log in normally (boring)</span>
+        </div>
       </div>
     </div>
   );
